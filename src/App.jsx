ab499d3a7c014a46957b1ab7040d764af3135d35
benidama <benidama02@gmail.com>
2024-12-16 21:43:40 +0000

    React-vite and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Reactus from "./Component/Reactus";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-3xl text-cyan-700">React JS</h1>
+      <Reactus />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Reactus.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reactus = () => {
+  return (
+    <div>
+      <h1 className="text-slate-600 text-2xl">I need to learn react JS</h1>
+    </div>
+  );
+};
+
+export default Reactus;
