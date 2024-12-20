69abcdddd0f195ffd7d19ad5c6db52e1c5259f6e
benidama <benidama02@gmail.com>
2024-12-20 20:00:54 +0000

    react list

--- src/Component/State.jsx
+++ src/Component/State.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function State() {
   const [count, setCount] = useState(0);
+  const arr = ["anni", "jone", "claude"];
   const update = () => setCount((v) => v + 1);
   return (
     <div>
@@ -10,6 +11,11 @@ function State() {
         I will always looking forward my real time to my job. Evenif it very
         hard but it possible for me and us, untill when I reach my all goals.
       </p>
+      <h2>
+        {arr.map((ah) => {
+          return <li key={ah.id}>{ah}</li>;
+        })}
+      </h2>
       <h1>{count}</h1>
       <button onClick={update}>Get value</button>
     </div>
