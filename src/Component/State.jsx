c1dc8cdb7910a45b7b5320f798b8d7b045721c1a
benidama <benidama02@gmail.com>
2024-12-22 12:28:36 +0000

    changes added

--- src/Component/State.jsx
+++ src/Component/State.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function State() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("Blue");
   const arr = ["anni", "jone", "claude"];
   const update = () => setCount((v) => v + 1);
   return (
@@ -21,6 +22,13 @@ function State() {
         sapiente aperiam nihil deleniti voluptatum corrupti aut hic, eum in.
         Nesciunt consequuntur possimus quis.
       </p>
+      <p>My favolite color is {color}</p>
+      <button
+        className="btn bg-blue-500 rounded p-1"
+        onClick={() => setColor(() => "Green")}
+      >
+        My color
+      </button>
       <h1>{count}</h1>
       <button onClick={update}>Get value</button>
     </div>
