87b8183b004650d0061314fb4f875c8b47b91413
benidama <benidama02@gmail.com>
2024-12-23 20:32:54 +0000

    Header styling

--- src/Component/Ref.jsx
+++ src/Component/Ref.jsx
@@ -9,6 +9,7 @@ function Ref() {
   }, [valeur]);
   return (
     <div>
+      <h1 className="text-center text-green-500">UseHook in react</h1>
       <h3>Learn how to use useRef react hook</h3>
       <input
         className="bg-slate-400 text-center"
