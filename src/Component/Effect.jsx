dce24f6a7f15b95b3bd701f32ac2d03ce7a3c272
benidama <benidama02@gmail.com>
2024-12-25 22:39:07 +0000

    Fetching successful

--- src/Component/Effect.jsx
+++ src/Component/Effect.jsx
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from "react";
 
-// const url = "https://jsonplaceholder.typicode.com/posts";
-
-// interface User {
-//     id: number;
-//     name: string;
-//     email: string;
-//   }
-
 function Effect() {
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   useEffect(() => {
     async function getData() {
-      const repons = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await repons.json();
-      setResults(data);
+      setLoading(true);
+      try {
+        const repons = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const data = await repons.json();
+        setResults(data);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
     }
     getData();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return (
+      <div className="text-red-600 text-2xl p-2">
+        Something went wrong, please double check
+      </div>
+    );
+  }
   return (
     <div>
       <h1 className="text-3xl text-green-950 m-3">Data fetching from API</h1>
