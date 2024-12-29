83e89a9b57f54add22a5f28f251d9e30f3dcc814
benidama <benidama02@gmail.com>
2024-12-29 19:14:57 +0000

    Reason  why friend decided to change his life style

--- src/Component/Ref.jsx
+++ src/Component/Ref.jsx
@@ -17,6 +17,11 @@ function Ref() {
         I am talking with my honey where she told me her love me very much,and I
         told her she is a beautiful more than other.
       </p>
+      <p>
+        One of my friend is already change his life journey from priesthood to
+        normal life. He told that he decided that decision because he need
+        freedom and real love from the family, friends and others.
+      </p>
       <h3>Learn how to use useRef react hook</h3>
       <input
         className=" text-center m-2 btn rounded-md p-2 text-white"
