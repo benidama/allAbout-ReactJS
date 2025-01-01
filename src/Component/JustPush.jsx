9f7a21ef68d7bce2f1d27c08e74f12514a8c3ba6
benidama <benidama02@gmail.com>
2025-01-01 22:19:19 +0000

    New file

---
+++ src/Component/JustPush.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function JustPush() {
+  return (
+    <div>
+      <h1>Hello guys, I love you all.</h1>
+      <p>The first day of 2025, I have a good day with all The Ben funs</p>
+    </div>
+  );
+}
+
+export default JustPush;
