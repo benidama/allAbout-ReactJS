57052b9f5e87507c5e5ae585f149fc3c5cb9421f
benidama <benidama02@gmail.com>
2025-01-03 21:21:48 +0000

    New year news

--- src/Component/JustPush.jsx
+++ src/Component/JustPush.jsx
@@ -4,7 +4,11 @@ function JustPush() {
   return (
     <div>
       <h1>Hello guys, I love you all.</h1>
-      <p>The first day of 2025, I have a good day with all The Ben funs</p>
+      <p>
+        The first day of 2025, I have a good day with all The Ben funs. This is
+        how I have starting my new year.But I was starting with sad news where
+        we loose our lovely family member.
+      </p>
     </div>
   );
 }
