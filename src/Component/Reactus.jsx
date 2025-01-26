feffdce9e451f2dadb845d3a41ef55b7987c5c18
benidama <benidama02@gmail.com>
2025-01-26 20:05:26 +0000

    adding changes

--- src/Component/Reactus.jsx
+++ src/Component/Reactus.jsx
@@ -4,6 +4,12 @@ const Reactus = () => {
   return (
     <div>
       <h1 className="text-slate-600 text-2xl">I need to learn react JS</h1>
+      <p className="text-white text-sm">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
+        explicabo modi, praesentium atque sapiente error facilis natus illum
+        fuga vel illo rem quibusdam.
+      </p>
+      <p>I need to know more about react js</p>
     </div>
   );
 };
